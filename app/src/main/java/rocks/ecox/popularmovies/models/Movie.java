1ab0b34e64331537fda843fa4aa798c37655603d
Erik Cox <erikbcox@gmail.com>
2016-10-17 15:33:02 -0700

    refactor: Organize models, utilities,  and adapters in packages

---
+++ app/src/main/java/rocks/ecox/popularmovies/models/Movie.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright (C) 2016 Erik Cox
+ */
+
+package rocks.ecox.popularmovies.models;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * Creates a movie object
+ */
+
+public class Movie implements Parcelable {
+    private String mTitle;
+    private String mPosterPath;
+    private String mBackdropPath;
+    private String mReleaseDate;
+    private Double mUserRating;
+    private String mSynopsis;
+
+    public String getTitle() { return mTitle; }
+    public String getPoster() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", mPosterPath);
+    }
+    public String getBackdrop() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", mBackdropPath);
+    }
+    public String getThumbnail() {
+        return String.format("https://image.tmdb.org/t/p/w92/%s", mPosterPath);
+    }
+    public String getReleaseDate() { return mReleaseDate; }
+    public String getSynopsis() {
+        return mSynopsis;
+    }
+    public Double getUserRating() {
+        if (this.mUserRating > 0) {
+            return mUserRating;
+        } else {
+            return 0.0;
+        }
+    }
+
+    /** Reformats TMDB release date from yyyy-MM-dd to MM-dd-yyyy */
+    private String formatReleaseDate(String releaseDate) throws ParseException {
+        if(releaseDate != null && !releaseDate.equals("")) {
+            SimpleDateFormat inFormat = new SimpleDateFormat("yyyy-MM-dd");
+            SimpleDateFormat outFormat = new SimpleDateFormat("MM-dd-yyyy");
+            Date d = inFormat.parse(releaseDate);
+            return outFormat.format(d);
+        } else {
+            return "Unknown";
+        }
+    }
+
+    public Movie(JSONObject jsonObject) throws JSONException, ParseException{
+        this.mPosterPath = jsonObject.getString("poster_path");
+        this.mBackdropPath = jsonObject.getString("backdrop_path");
+        this.mTitle = jsonObject.getString("original_title");
+        this.mSynopsis = jsonObject.getString("overview");
+        this.mUserRating = jsonObject.getDouble("vote_average");
+        this.mReleaseDate = formatReleaseDate(jsonObject.getString("release_date"));
+    }
+
+    public static ArrayList<Movie> fromJSONArray(JSONArray array) {
+        ArrayList<Movie> results = new ArrayList<>();
+
+        for (int x = 0; x < array.length(); x++) {
+            try {
+                results.add(new Movie(array.getJSONObject(x)));
+            } catch (JSONException | ParseException e) {
+                e.printStackTrace();
+            }
+        }
+        return results;
+    }
+
+    /** Creates a Movie object  from a parcel*/
+    private Movie(Parcel parcel){
+        mTitle = parcel.readString();
+        mPosterPath = parcel.readString();
+        mBackdropPath = parcel.readString();
+        mReleaseDate = parcel.readString();
+        mUserRating = parcel.readDouble();
+        mSynopsis = parcel.readString();
+    }
+    
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    /** Creates a parcel from a Movie object */
+    public void writeToParcel(Parcel parcel, int i){
+        parcel.writeString(mTitle);
+        parcel.writeString(mPosterPath);
+        parcel.writeString(mBackdropPath);
+        parcel.writeString(mReleaseDate);
+        parcel.writeDouble(mUserRating);
+        parcel.writeString(mSynopsis);
+    }
+
+    public static final Parcelable.Creator<Movie> CREATOR = new Parcelable.Creator<Movie>(){
+        @Override
+        public Movie createFromParcel(Parcel source) {
+            return new Movie(source);
+        }
+
+        @Override
+        public Movie[] newArray(int size) {
+            return new Movie[size];
+        }
+    };
+
+}
---
+++ app/src/main/java/rocks/ecox/popularmovies/utilities/Utility.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2016 Erik Cox
+ */
+
+package rocks.ecox.popularmovies.utilities;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import static android.content.Context.MODE_PRIVATE;
+
+/**
+ * A class of common tasks
+ */
+
+public class Utility {
+
+    /** Gets the value of the sortBy sharedPreference */
+    public static String getSortKey(Activity activity) {
+        SharedPreferences mPref = activity.getSharedPreferences("MoviePrefs", MODE_PRIVATE);
+        return mPref.getString("SortBy", "popular");
+    }
+
+    /** Saves the value of sortBy to a sharedPreference */
+    public static void setSortKey(Activity activity, String choice) {
+        SharedPreferences mPref = activity.getSharedPreferences("MoviePrefs", MODE_PRIVATE);
+        SharedPreferences.Editor mEditor = mPref.edit();
+        mEditor.putString("SortBy",choice);
+        mEditor.apply();
+    }
+
+    /** Checks to see if a network connection is available */
+    public static boolean isOnline(Context context) {
+        ConnectivityManager mCm =
+                (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo mNetInfo = mCm.getActiveNetworkInfo();
+        return mNetInfo != null && mNetInfo.isConnected();
+    }
+}
